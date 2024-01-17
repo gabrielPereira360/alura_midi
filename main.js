fa63e387888886182dfe08c09e4a92943e22556c
Gabriel Pereira <gabriel.obom01@gmail.com>
2024-01-17 00:47:26 -0300

    Otimizando o código v1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.querySelector('.tecla_pom');
+// Obtem todos os elementos dos botões
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function  tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+// Percorrer a Lista
+let i = 0;
+while (i < listaDeTeclas.length){
+    listaDeTeclas[i].onclick = tocaSomPom;
+    i++;
+    console.log(i);
+}
