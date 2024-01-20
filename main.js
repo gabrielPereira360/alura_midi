23db809a162010e79e250748c1767148e9584607
Gabriel Pereira <gabriel.obom01@gmail.com>
2024-01-20 02:15:17 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-// Obtem todos os elementos dos botões
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 function tocaSom(seletorAudio) {
-    const elemento = document.querySelector(seletorAudio); // Obtem os dados do elemento
-    if (elemento && elemento.localName === 'audio') { // Verifica se elemento é nulo e se é um audio   
+    const elemento = document.querySelector(seletorAudio); 
+    if (elemento && elemento.localName === 'audio') { 
         elemento.play();
     }else {
         console.log('Elemento não encontrado ou seletor inválido!');
     }
 }
 
-// Percorrer a Lista
 for (let i = 0; i < listaDeTeclas.length; i++) {
-    const tecla = listaDeTeclas[i]; // Configura a tecla
-    const instrumento = tecla.classList.item(1); // Configura o instrumento
-    const idAudio = `#som_${instrumento}`; // Configura o id do Audio completo
-tecla.onkeydown = function (teclado) { // Evento que vai capturar as informações da tecla no teclado
+    const tecla = listaDeTeclas[i]; 
+    const instrumento = tecla.classList.item(1); 
+    const idAudio = `#som_${instrumento}`; 
+tecla.onkeydown = function (teclado) {
         if (teclado.code === "Enter" || teclado.code === "Space") {
             listaDeTeclas[i].classList.add('ativa');
         }
     };
     tecla.onclick = function () {
-        tocaSom(idAudio); // toca o som do instrumento de acordo com a tecla
+        tocaSom(idAudio); 
     };
     tecla.onkeyup = function () {
         listaDeTeclas[i].classList.remove('ativa');
