6a76dc15213f89cd6d0d8b4fd18a54fa8e7c7edd
Gabriel Pereira <gabriel.obom01@gmail.com>
2024-01-20 02:13:07 -0300

    Projeto completo

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Olá, bem vindo ao projeto Alura Midi desenvolvido como forma de aprendizado para o desenvolvimento de funções por meio da linguagem Javascript.
 
 ## Prévias
+
+<div style="display: flex; gap: 50px">
 - Desktop: 
-Nada ainda 
+<img src="https://i.ibb.co/sQY29zR/127-0-0-1-5500-Nest-Hub-Max.png" alt="Página principal da página alura MIDI" width="350px">
+<img src="https://i.ibb.co/JB9D073/127-0-0-1-5500-Nest-Hub-Max-1.png" alt="Página principal da página alura MIDI com uma tecla pressionada" width="350px">
+</div>
 
 ## Ferramentas utilizadas:
 
@@ -16,6 +20,6 @@ Nada ainda
 
 ### Gabriel Pereira
 
-### Link da prévia: Nada ainda
+### Link da prévia: https://gabrielpereira360.github.io/alura_midi/
 
 ### Linkedin: https://www.linkedin.com/in/gabriel-martins2003/
--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 // Obtem todos os elementos dos botões
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function  tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play(); // Faz a reprodução do som
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio); // Obtem os dados do elemento
+    if (elemento && elemento.localName === 'audio') { // Verifica se elemento é nulo e se é um audio   
+        elemento.play();
+    }else {
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 // Percorrer a Lista
-for (let i = 0; i < listaDeTeclas.length; i++){
+for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i]; // Configura a tecla
     const instrumento = tecla.classList.item(1); // Configura o instrumento
     const idAudio = `#som_${instrumento}`; // Configura o id do Audio completo
-    tecla.onclick = function (){
+tecla.onkeydown = function (teclado) { // Evento que vai capturar as informações da tecla no teclado
+        if (teclado.code === "Enter" || teclado.code === "Space") {
+            listaDeTeclas[i].classList.add('ativa');
+        }
+    };
+    tecla.onclick = function () {
         tocaSom(idAudio); // toca o som do instrumento de acordo com a tecla
     };
+    tecla.onkeyup = function () {
+        listaDeTeclas[i].classList.remove('ativa');
+    };
+
 }
